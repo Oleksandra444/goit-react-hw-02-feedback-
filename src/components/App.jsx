6b09c601fb6eac7dc6d15fc9e45eb51100191998
Oleksandra444 <oleksandra.work.4.4.4@gmail.com>
2023-10-31 21:13:16 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import { SectionTitle } from "./SectionTitle/SectionTitle";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Statistic} from "./Statistic/Statistic";
+import { Statistic } from "./Statistic/Statistic";
+import { NotificationMessage } from "./NotificationMessage/NotificationMessage";
 
 export class App extends Component  {
   state = { 
@@ -47,7 +48,7 @@ export class App extends Component  {
         <SectionTitle title='Please leave feedback'>
           <FeedbackOptions options={ Object.keys(this.state)}  onLeaveFeedback={this.onLeaveFeedback}/>
         </SectionTitle>
-        <SectionTitle title='Statistics'> <Statistic options={Object.entries(this.state)} total={totalFeedback} positivePercentage={positiveFeedback} /></SectionTitle>
+        <SectionTitle title='Statistics'> {totalFeedback?<Statistic options={Object.entries(this.state)} total={totalFeedback} positivePercentage={positiveFeedback} /> : <NotificationMessage/>} </SectionTitle>
       </div>
     );
   }
---
+++ src/components/NotificationMessage/NotificationMessage.jsx
@@ -0,0 +1,7 @@
+export const NotificationMessage = () => {
+    return (
+        <div>
+            <p>There is no feedback</p>
+    </div>
+)
+}
